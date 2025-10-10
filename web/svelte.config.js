45b66cefccab129cd2b7e8984174d32eb4067420
eastoid <stromkoza1@gmail.com>
2025-10-10 17:40:34 +0200

    Remove leftover arg

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -27,6 +27,7 @@ const config = {
                 w.message.includes("event_directive_deprecated")
                 || w.message.includes("css_unused_selector") 
                 || w.message.includes("a11y_no_static_element_interactions")
+                || w.message.includes("a11y_click_events_have_key_events")
             ) return false
 			return true
 		},
