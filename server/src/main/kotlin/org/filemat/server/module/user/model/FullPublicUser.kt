6c0d100d508234da8913b2078a7436d89b6753b9
mungo <stromkoza1@gmail.com>
2026-02-10 15:50:03 +0100

    server: cleanup

--- server/src/main/kotlin/org/filemat/server/module/user/model/FullPublicUser.kt
+++ server/src/main/kotlin/org/filemat/server/module/user/model/FullPublicUser.kt
@@ -3,9 +3,6 @@ package org.filemat.server.module.user.model
 import com.github.f4b6a3.ulid.Ulid
 import kotlinx.serialization.Serializable
 import org.filemat.server.config.UlidSerializer
-import org.springframework.data.annotation.Id
-import org.springframework.data.relational.core.mapping.Column
-import org.springframework.data.relational.core.mapping.Table
 
 @Serializable
 data class FullPublicUser(
