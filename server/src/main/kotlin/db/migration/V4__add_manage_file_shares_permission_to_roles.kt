50b94d94f41a3318a51fcedb5c0ed48dbf14f900
mungo <stromkoza1@gmail.com>
2026-02-13 17:48:29 +0100

    server: improve V4 db migration

--- server/src/main/kotlin/db/migration/V4__add_manage_file_shares_permission_to_roles.kt
+++ server/src/main/kotlin/db/migration/V4__add_manage_file_shares_permission_to_roles.kt
@@ -2,7 +2,6 @@ package db.migration
 
 import org.filemat.server.module.permission.model.SystemPermission
 import org.filemat.server.module.permission.model.serialize
-import org.filemat.server.module.role.model.Role
 import java.sql.Statement
 
 class V4__add_manage_file_shares_permission_to_roles : Migration() {
@@ -18,28 +17,34 @@ class V4__add_manage_file_shares_permission_to_roles : Migration() {
         SystemPermission.SUPER_ADMIN,
     )
 
-    val newPermissions = systemPermissions.toMutableSet().also { it.add(SystemPermission.MANAGE_ALL_FILE_SHARES) }
-    val newPermissionsSerialized = newPermissions.serialize()
+    val newPermission = SystemPermission.MANAGE_ALL_FILE_SHARES
 
     override fun run(st: Statement) {
-        val roleResults = st.executeQuery("SELECT * FROM role")
-        val roles = mutableListOf<Role>()
+        val roleResults = st.executeQuery("SELECT role_id, permissions FROM role")
+
+        // UserID + PermissionList
+        val updatedRows = mutableListOf<Pair<String, String>>()
+
         while (roleResults.next()) {
             val roleId = roleResults.getString("role_id")
             val permissionsStr = roleResults.getString("permissions")
 
             if (roleId != null && permissionsStr != null) {
-                val permissions = SystemPermission.fromString(permissionsStr)
-                val hasAll = permissions.size == systemPermissions.size
-
-                if (hasAll) {
-                    try {
-                        st.execute("UPDATE role SET permissions = '$newPermissionsSerialized' WHERE role_id = '${roleId}'")
-                    } catch (e: Exception) {
-                        println("Migration V4 failed to update permissions of role with role ID '$roleId'")
-                        e.printStackTrace()
-                    }
+                val permissions = SystemPermission.fromString(permissionsStr).toMutableSet()
+                if (permissions.containsAll(systemPermissions)) {
+                    permissions.add(newPermission)
+                    val serialized = permissions.serialize()
+                    updatedRows.add(roleId to serialized)
                 }
+            }
+        }
+
+        updatedRows.forEach { (roleId, newPermissions) ->
+            try {
+                st.execute("UPDATE role SET permissions = '$newPermissions' WHERE role_id = '$roleId'")
+            } catch (e: Exception) {
+                println("Migration V4 failed to update permissions of role with role ID '$roleId'")
+                e.printStackTrace()
             }
         }
     }
